a3a7a43dc2edc05a0ef0d0d140309130b7eb57d8
Johnson <hungjiewu@gmail.com>
2020-10-23 00:22:18 +0800

    Initial Commit

---
+++ 00757001-10years/_0757001_10yearsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0757001_10yearsApp.swift
+//  00757001-10years
+//
+//  Created by User04 on 2020/10/22.
+//
+
+import SwiftUI
+
+@main
+struct _0757001_10yearsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
